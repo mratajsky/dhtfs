932e1934ee3d248f3b71a074b278be9de2a746f5
Michal Ratajsky <michal.ratajsky@gmail.com>
2019-04-16 19:46:18 +0200

    Update dependencies and .gitignore

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 
 from setuptools import setup
 
-setup(name='dhtfs-peer',
+setup(name='dhtfs',
       version='1.0',
-      description='DHT peer for dhtfs',
-      scripts=['bin/dhtfs-peer'],
+      description='DHT tool and peer for dhtfs',
+      scripts=['bin/dhtfs', 'bin/dhtfs-peer', 'bin/dhtfs-spawn-peers'],
       packages=['dhtfs'],
       zip_safe=False)
