2d7573f11ebd6a07d701bee712a360482ed7148b
Artur <e2001543@ceng.metu.edu.tr>
2019-05-03 14:48:25 +0200

    Added proper utility functions for indexing

--- dhtfs/utils.py
+++ dhtfs/utils.py
@@ -1,8 +1,12 @@
 import logging
 from collections import namedtuple
+from decimal import Decimal
 
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
+
+#TO-DO,   max depth of tree variable
+TREE_DEPTH = 30
 
 __all__ = ['defaults',
            'setup_logging']
@@ -61,3 +65,46 @@ def naming_func(label):
             break
     
     return label[:i+1]
+
+
+
+def float_to_bin_no_dot(f, digits=10):
+    '''convert float to bin without decimal point(1.5 -> 1.1-> 11), f in [0]'''
+    f = Decimal(f)
+    f = f'{f:f}'
+    intPart, decimalPart = f.split('.')
+    decimalPart = '0.' + decimalPart
+
+    result = bin(int(intPart))[2:]
+    for x in range(digits):
+        temp = float(decimalPart) * 2
+        intTmp, decimalPart = f'{Decimal(temp):f}'.split('.')
+        result = result + intTmp
+        decimalPart = '0.' + decimalPart
+
+    return result
+
+
+
+def get_label(range_min,range_max,total_len): 
+    '''get label of bucket'''
+    rMin = range_min*1.0/total_len
+    rMax = range_max*1.0/total_len
+    binMin = float_to_bin_no_dot(rMin,TREE_DEPTH)
+    binMax = float_to_bin_no_dot(rMax,TREE_DEPTH)
+
+    
+    if(binMax[0] == '1'):
+        binMax = '0'
+        for i in range(TREE_DEPTH):
+            binMax = binMax + '1'
+
+    label = '#'
+
+    for i in range(TREE_DEPTH+1):
+        if(binMin[i] == binMax[i]):
+            label = label + binMin[i]
+        else:
+            break
+    
+    return label
