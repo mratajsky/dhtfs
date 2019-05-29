aa9ec96e0fe457e042a2ccdef6c6f5635a989b2a
Artur <e2001543@ceng.metu.edu.tr>
2019-05-29 15:04:51 +0200

    Add with index table, lookup functions

--- dhtfs/peer/rpc.py
+++ dhtfs/peer/rpc.py
@@ -4,12 +4,13 @@ import os
 import queue
 import threading
 from contextlib import contextmanager
+import random
 
 from kademlia.utils import digest
 
 from thrift.transport import TSocket
 from thrift.transport import TTransport
-from thrift.protocol import TBinaryProtocol
+from thrift.protocol import TBinaryProtocol, TCompactProtocol
 from thrift.server import TServer
 
 from ..thrift.rpc.Rpc import Processor
@@ -51,13 +52,13 @@ class Handler:
         return [Peer(n.ip, n.port) for n in result]
 
     def _find_search_keys(self, key):
-        closest = self._find_closest_peers(dht_key)
+        closest = self._find_closest_peers(key)
         for peer in closest:
             client = Client(peer.host, peer.port)
             client.connect()
             try:
-                bucket_keys = client.GetBucketKeys(digest(key))
-                return bucket_keys.search_key_min, bucket_keys.search_key_max
+                bucket_keys = client.GetBucketKeys(key)
+                return bucket_keys
             except StorageException:
                 pass
 
@@ -67,29 +68,76 @@ class Handler:
         label = float_to_bin_no_dot(search_key_to_interval(search_key))
 
         while( upper_bound >= lower_bound):
-            mid_point = (lower_bound + upper_bound) / 2
+            mid_point = (lower_bound + upper_bound) // 2
             prefix = label[0:mid_point]
             dht_key = naming_func(f'{ident}:{prefix}')
 
-            search_key_min, search_key_max = self._find_search_keys(dht_key)
-            if search_key_min is None:
-               upper_bound = len(dht_key)
+            bucket_keys = self._find_search_keys(digest(dht_key))
+            print(f'used: {dht_key}, got: {bucket_keys}')
+           
+            if bucket_keys is None:
+                if upper_bound == len(prefix):
+                    break
+                upper_bound = len(prefix)
             else:
-                if(search_key >= search_key_min and search_key <= search_key_max):
+                key_min, key_max = bucket_keys.search_key_min, bucket_keys.search_key_max
+                if(search_key >= key_min and search_key < key_max):
                     return dht_key
                 lower_bound = len(next_naming_func(prefix, label))
 
-        return naming_func(f'{ident}:#')
+        print(f'return {ident}:#')
+        return f'{ident}:#'
 
-        # bucket = self.get_iterative(dht_key)
-        # if bucket is not None:
-        #     bucket = thrift_unserialize(bucket, Bucket())
-        #     if(search_key >= bucket.search_key_min and search_key <= bucket.search_key_max):
-        #         return dht_key
-        #     else:
-        #         lower_bound = len(next_naming_func(prefix, label))
-        # else:
-        #    upper_bound = len(dht_key)
+    def bucketSplitter(self, bucket, name):
+        # split
+        print(f"Splitter {name}")
+
+        midPoint = (bucket.search_key_max + bucket.search_key_min) // 2 
+        if midPoint * 2 < bucket.search_key_max:
+            midPoint = midPoint + 1
+        bucketLeft = Bucket(search_key_min=bucket.search_key_min,
+                            search_key_max=midPoint,
+                            values = [])
+        bucketRight = Bucket(search_key_min=midPoint,
+                            search_key_max=bucket.search_key_max,
+                            values = []) 
+        for x in bucket.values:
+            if x.search_key < midPoint:
+                bucketLeft.values.append(x)
+            else:
+                bucketRight.values.append(x) 
+        
+
+        label = get_label(bucket.search_key_min, bucket.search_key_max)
+        print(f"Label: {label}")
+        dht_key = digest(f'{name}:{label}')
+        peers = self.FindClosestPeers(dht_key)
+        indx = random.randint(0,len(peers)-1)
+        client = Client(peers[indx].host, peers[indx].port)
+        client.connect()
+        print('==')
+
+        if label[-1] == '0':
+            if len(bucketLeft.values) > DEFAULT_BUCKET_SIZE:
+                print(f'here1 {client}')
+                client.Put(dht_key, thrift_serialize(bucketRight))
+                print('here2')
+                return self.bucketSplitter(bucketLeft, name)
+            if len(bucketRight.values) > DEFAULT_BUCKET_SIZE:
+                client.Put(dht_key, thrift_serialize(self.bucketSplitter(bucketRight,name)))
+                return bucketLeft
+            print('base')
+            client.Put(dht_key, thrift_serialize(bucketRight))
+            return bucketLeft
+        else:
+            if len(bucketRight.values) > DEFAULT_BUCKET_SIZE:
+                client.Put(dht_key, thrift_serialize(bucketLeft))
+                return self.bucketSplitter(bucketRight,name)
+            if len(bucketLeft.values) > DEFAULT_BUCKET_SIZE:
+                client.Put(dht_key, thrift_serialize(self.bucketSplitter(bucketLeft,name)))
+                return bucketRight
+            client.Put(dht_key, thrift_serialize(bucketLeft))
+            return bucketRight
 
 
     def GetBucketKeys(self, key):
@@ -101,14 +149,21 @@ class Handler:
         logger.debug(f"RPC: Put({key.hex()}, ...)")
         self._db.put(key, value)
 
-    def Add(self, key, value: BucketValue, search_key_min, search_key_max):
+    def Add(self, key, value: BucketValue, name, search_key_min, search_key_max):
         logger.debug(f"RPC: Add({key.hex()}, {search_key_min}, {search_key_max}, ...)")
         with self._lock_bucket(key):
             bucket = self._db.get(key)
             if bucket is not None:
                 bucket = thrift_unserialize(bucket, Bucket())
-                # TODO: this is not total ordering
+                # TODO: fix splitting full atomic buckets
+                if (bucket.search_key_min + 1) >= bucket.search_key_max:
+                    return
+                 # TODO: this is not total ordering
                 self.insort_right(bucket.values, value)
+                if len(bucket.values) > DEFAULT_BUCKET_SIZE:
+                    print(1)
+                    bucket = self.bucketSplitter(bucket, name)                    
+                    print(2)
             else:
                 bucket = Bucket(search_key_min=search_key_min,
                                 search_key_max=search_key_max,
@@ -135,21 +190,102 @@ class Handler:
             raise StorageException(404, 'No matching value found')
         return bucket.values[idx - 1]
 
-    def GetRange(self, key, search_key_min, search_key_max):
-        logger.debug(f"RPC: GetRange({key.hex()}, {search_key_min}, {search_key_max})")
-        try:
-            bucket = self._get_nonempty_bucket(key)
-            idx_min = self.bisect_left(bucket.values, search_key_min)
-            if idx_min == len(bucket.values):
-                return []
-            idx_max = self.bisect_right(bucket.values, search_key_max)
-            if idx_max == 0:
-                return []
-            return bucket.values[idx_min:idx_max]
-        except StorageException:
-            # Empty bucket
-            return []
+    # def GetRange(self, key, search_key_min, search_key_max):
+    #     logger.debug(f"RPC: GetRange({key.hex()}, {search_key_min}, {search_key_max})")
+    #     try:
+    #         bucket = self._get_nonempty_bucket(key)
+    #         idx_min = self.bisect_left(bucket.values, search_key_min)
+    #         if idx_min == len(bucket.values):
+    #             return []
+    #         idx_max = self.bisect_right(bucket.values, search_key_max)
+    #         if idx_max == 0:
+    #             return []
+    #         return bucket.values[idx_min:idx_max]
+    #     except StorageException:
+    #         #Empty bucket
+    #         return []
 
+    def RangeRecursiveForward(self, bucket, name, search_key_min, search_key_max):
+        bLabel = get_label(bucket.search_key_min, bucket.search_key_max)
+        result = []
+        for item in bucket.values:
+            if item.search_key >= search_key_min and item.search_key <= search_key_max:
+                result = result + item
+        while True:
+            if bLabel[-1] == '1' and bLabel[-2] == '1':
+                bLabel = get_left_neighbour(bLabel)
+            else:
+                bLabel = get_right_neighbour(bLabel)
+            inteval = get_label_range(bLabel)
+            if inteval[0] > search_key_max or inteval[1] <= search_key_min:  # intersection is NULL, stop recursion & iteration
+                return result
+            elif inteval[0] >= search_key_min and inteval[1] - 1 <= search_key_max:   # Range totaly covers the interval, recurse down, then iterate left/right
+                
+                dht_key = digest(f'{name}:{naming_func(bLabel)}')
+                peers = self.FindClosestPeers(dht_key)
+                nextBucket = None
+                for peer in peers:
+                    client = Client(peer.host, peer.port)
+                    client.connect()
+                    try:
+                        nextBucket = client.Get(dht_key)
+                        break
+                    except:
+                        pass
+                if nextBucket is None:
+                    print("NOT GOOD, Recursive forward ERROR!!!")
+                    return result
+                else:
+                    result = result + self.RangeRecursiveForward(nextBucket,name,inteval[0],inteval[1])
+            else:   #range partially covers , recurse down, stop iterating
+                dht_key = digest(f'{name}:{bLabel}')
+                peers = self.FindClosestPeers(dht_key)
+                nextBucket = None
+                for peer in peers:
+                    client = Client(peer.host, peer.port)
+                    client.connect()
+                    try:
+                        nextBucket = client.Get(dht_key)
+                        break
+                    except:
+                        pass
+                if nextBucket is None:
+                    dht_key = digest(f'{name}:{naming_func(bLabel)}')
+                    peers = self.FindClosestPeers(dht_key)
+                    nextBucket = None
+                    for peer in peers:
+                        client = Client(peer.host, peer.port)
+                        client.connect()
+                        try:
+                            nextBucket = client.Get(dht_key)
+                            break
+                        except:
+                            print("NOT GOOD, Recursive forward ERROR 2  !!!")
+                            pass
+                result = result + self.RangeRecursiveForward(nextBucket,name,max(inteval[0],search_key_min),min(inteval[1]-1,search_key_max))
+                return result
+        
+    def GetRange(self, key, search_key_min, search_key_max): 
+        # TO-DO  chenger variable key to name/ident (need thrift update)
+        label = self._lookup(key, search_key_min)
+        print(f'key: {key}   Range label:  {label}')
+        dht_key = digest(label)
+        closest = self._find_closest_peers(dht_key)
+        bucket = None
+        print('1')
+        for peer in closest:
+            client = Client(peer.host, peer.port)
+            client.connect()
+            try:
+                bucket = client.Get(dht_key)
+                if bucket is not None:
+                    print("did it!")
+                    break
+            except StorageException:
+                pass
+        
+        return self.RangeRecursiveForward(bucket, key, search_key_min, search_key_max)
+ 
     def _get_nonempty_bucket(self, key) -> Bucket:
         bucket = self._db.get(key)
         if bucket is not None:
@@ -259,11 +395,12 @@ class RPC:
             handler = Handler(self._db, self._dht_pipe, results, condition)
             processor = Processor(handler)
             transport = TSocket.TServerSocket(host=self._host, port=self._port)
-            tfactory = TTransport.TBufferedTransportFactory()
-            pfactory = TBinaryProtocol.TBinaryProtocolAcceleratedFactory()
+            tfactory = TTransport.TFramedTransportFactory()
+            pfactory = TCompactProtocol.TCompactProtocolAcceleratedFactory()
 
             server = TServer.TThreadPoolServer(
                 processor, transport, tfactory, pfactory, daemon=True)
+            server.setNumThreads(100)
             server.serve()
         threading.Thread(target=server, daemon=True).start()
 
--- dhtfs/utils.py
+++ dhtfs/utils.py
@@ -7,10 +7,11 @@ from thrift.protocol import TBinaryProtocol
 
 #TO-DO,   max depth of tree variable, search window
 DEFAULT_TREE_DEPTH = 32
-DEFAULT_SEARCH_KEY_MAX = 864000 #seconds granularity, 10 days period
+DEFAULT_SEARCH_KEY_MAX = 100 #4294967296 #seconds granularity, 10 days period
+DEFAULT_BUCKET_SIZE = 5
 
-__all__ = ['defaults',
-           'setup_logging']
+# __all__ = ['defaults',
+#            'setup_logging']
 
 DEFAULT_HOST = '127.0.0.1'
 DEFAULT_PORT = 9090
@@ -78,14 +79,14 @@ def naming_func(label):
 
 def float_to_bin_no_dot(f, digits=DEFAULT_TREE_DEPTH):
     '''convert float to bin without decimal point(1.5 -> 1.1-> 11), f in [0]'''
-    f = Decimal(f)
+    f = Decimal(str(f))
     f = f'{f:f}'
     intPart, decimalPart = f.split('.')
     decimalPart = '0.' + decimalPart
 
     result = bin(int(intPart))[2:]
     for x in range(digits):
-        temp = float(decimalPart) * 2
+        temp = str(float(decimalPart) * 2)
         intTmp, decimalPart = f'{Decimal(temp):f}'.split('.')
         result = result + intTmp
         decimalPart = '0.' + decimalPart
@@ -96,25 +97,57 @@ def search_key_to_interval(search_key, range_max=DEFAULT_SEARCH_KEY_MAX):
     '''maps search key(time stamp) to the timing interval, returns between [0,1]'''
     return (search_key*1.0/range_max)
 
-def get_label(range_min,range_max,total_len): 
+def get_label(range_min,range_max,total_len=DEFAULT_SEARCH_KEY_MAX): 
     '''get label of bucket'''
+    print(f"GET Label MIN: {range_min}  MAX: {range_max}")
     rMin = range_min*1.0/total_len
-    rMax = range_max*1.0/total_len
-    binMin = float_to_bin_no_dot(rMin,TREE_DEPTH)
-    binMax = float_to_bin_no_dot(rMax,TREE_DEPTH)
+    rMax = (range_max-1)*1.0/total_len
+    binMin = float_to_bin_no_dot(rMin,DEFAULT_TREE_DEPTH)
+    binMax = float_to_bin_no_dot(rMax,DEFAULT_TREE_DEPTH)
 
     
-    if(binMax[0] == '1'):
-        binMax = '0'
-        for i in range(TREE_DEPTH):
+    if(binMax[1] == '1'):
+        binMax = '#0'
+        for i in range(DEFAULT_TREE_DEPTH):
             binMax = binMax + '1'
 
-    label = '#'
+    label = ""
 
-    for i in range(TREE_DEPTH+1):
+    for i in range(DEFAULT_TREE_DEPTH+1):
         if(binMin[i] == binMax[i]):
             label = label + binMin[i]
         else:
             break
     
     return label
+
+def get_right_neighbour(label):
+    if naming_func(label) == '#0':
+        return label
+    for i in range(len(label)-1,-1,-1):
+        if label[i] == '0':
+            break
+    return label[:i] + '1'
+
+
+def get_left_neighbour(label):
+    if naming_func(label) == '#':
+        return label
+    for i in range(len(label)-1,-1,-1):
+        if label[i] == '1':
+            break
+    return label[:i] + '0'
+
+def get_label_range(label):
+    lower = 0
+    higher = 4294967296
+    for i in range(2,len(label)):
+        midPoint = (lower + higher) // 2
+        if midPoint * 2 < higher:
+            midPoint = midPoint + 1
+        if label[i] == '0':
+            higher = midPoint
+        else:
+            lower = midPoint
+    return [lower,higher]
+
